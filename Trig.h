b92d6118c3b9e7dae0abd1021a9ab6dd1d1d5414
Brett <brettpokemon@gmail.com>
2025-07-14 19:02:43 -0400

    Obj
    Added obj's fixed vectors

--- Trig.h
+++ Trig.h
@@ -11,7 +11,9 @@ this file is for reuseable vector math caluclations
 
 #include <cstdlib>
 #include <math.h>
-namespace Trig
+
+
+namespace GameObject 
 {
 
     struct Vec2
@@ -28,7 +30,76 @@ namespace Trig
 
     };
 
+    struct SimpleObject
+    {
+        SimpleObject()
+        {
+            position = nullptr;
+            size = nullptr;
+        }
 
+        ~SimpleObject()
+        {
+            delete position;
+            delete size;
+        }
+
+        void SetPosition(float x, float y)
+        {
+            position = new Vec2(x, y);
+        }
+
+        void SetCenter(SimpleObject& obj)
+        {
+            float halfWidth = obj.size->x / 2;
+            float halfHeight = obj.size->y / 2;
+            
+            float centerX = obj.position->x + halfWidth;
+            float centerY = obj.position->y + halfHeight;
+
+            obj.center = new Vec2(centerX, centerY);
+
+        }
+
+        Vec2 GetPosition()
+        {
+            return *position;
+        }
+
+        void SetSize(float width, float height)
+        {
+            size = new Vec2(width, height);
+        }
+
+        Vec2 GetSize()
+        {
+            return *size;
+        }
+
+        void SetRadius(float value)
+        {
+            radius = value;
+        }
+
+        float GetRadius()
+        {
+            return radius;
+        }
+
+        Vec2* position;
+        Vec2* size;
+
+        Vec2* center;
+
+        float radius;
+    };
+
+}
+
+
+
+namespace Trig
+{
 
     struct Pythagorian
     {
@@ -43,28 +114,28 @@ namespace Trig
         }
 
 
-        float CalculateDistance(Vec2& obj1, Vec2& obj2)
+        float CalculateDistance(GameObject::SimpleObject& obj1, GameObject::SimpleObject& obj2)
         {
-            float a = abs(obj1.x - obj2.x);
-            float b = abs(obj1.y - obj2.y);
+            float a = abs(obj1.position->x - obj2.position->x);
+            float b = abs(obj1.position->y - obj2.position->y);
 
             float c = sqrt((a * a) + (b * b));
             return c;
         }
 
-        float CalculcateAngle(Vec2 obj1, Vec2 obj2)
+        float CalculcateAngle(GameObject::SimpleObject& obj1, GameObject::SimpleObject& obj2)
         {
-            float a = abs(obj1.x - obj2.x);
-            float b = abs(obj1.y - obj2.y);
+            float a = abs(obj1.position->x - obj2.position->x);
+            float b = abs(obj1.position->y - obj2.position->y);
 
             float angle = atan2(b, a);
             return angle;
         }
 
-        bool RadialCheck(float r1, float r2, Vec2& obj1, Vec2& obj2)
+        bool RadialCheck(GameObject::SimpleObject& obj1, GameObject::SimpleObject& obj2)
         {
             float distance  = CalculateDistance(obj1, obj2);
-            float sumRadial = r1 + r2;
+            float sumRadial = obj1.radius + obj2.radius;
 
             if(sumRadial < distance)
             {
@@ -77,26 +148,26 @@ namespace Trig
 
         }
 //redo rectanglular check, add vec
-        bool RectangularCheck(float centerPointA, float centerPointB, float widthA, float widthB, float heightA, float heightB)
+        bool RectangularCheck(GameObject::SimpleObject& obj1, GameObject::SimpleObject& obj2)
         {
-            float halfWidthA = widthA / 2;
-            float halfWidthB = widthB / 2;
+            float halfWidthA = obj1.size->x / 2;
+            float halfWidthB = obj2.size->x / 2;
 
-            float halfHeightA = heightA / 2;
-            float halfHeightB = heightB / 2;
+            float halfHeightA = obj2.size->y / 2;
+            float halfHeightB = obj2.size->y / 2;
 
 
-            float leftEdgeA = centerPointA - halfWidthA;
-            float rightEdgeA = centerPointA + halfWidthA;
+            float leftEdgeA = obj1.center->x - halfWidthA;
+            float rightEdgeA = obj1.center->x  + halfWidthA;
 
-            float topEdgeA = centerPointA - halfWidthA;//minus because SFML uses - as Up + as down
-            float bottomEdgeA = centerPointA + halfWidthA;
+            float topEdgeA = obj1.center->y  - halfWidthA;//minus because SFML uses - as Up + as down
+            float bottomEdgeA = obj1.center->y + halfWidthA;
 
-            float leftEdgeB = centerPointB - halfWidthB;
-            float rightEdgeB = centerPointB + halfWidthB;
+            float leftEdgeB = obj2.center->x  - halfWidthB;
+            float rightEdgeB = obj2.center->x  + halfWidthB;
 
-            float topEdgeB = centerPointB - halfWidthB;
-            float bottomEdgeB = centerPointB + halfWidthB;
+            float topEdgeB = obj2.center->y - halfWidthB;
+            float bottomEdgeB = obj2.center->y + halfWidthB;
 
             if(rightEdgeA < leftEdgeB)
             {
